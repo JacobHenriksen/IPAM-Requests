647a98bd85f61228c5d537d1aacb9ece2c85ad7f
HENRIKSEN Jacob <jacob.henriksen.ext@sca.com>
2023-12-21 13:56:08 +0100

    User auth working.

--- ipam_requests.py
+++ ipam_requests.py
@@ -1,20 +1,32 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from getpass import getpass
+from getpass import getuser
 
 ##  DISABLE WARNINGS
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-##  DEFINE URLS AND USER CREDENTIALS
-URL = 'https://ipam.sca.com/'
-USERNAME = ''
-PASSWORD = ''
+##  DEFINE URLS
+URL = 'https://ipam.sca.com'
+TOKEN_PATH = '/api/ipmgr/user'
+USERNAME = 'jacobapi'
 
+##  RETRIEVE TOKEN
+print('\nRetrieving token...\n')
+post_response = requests.post(
+    URL+TOKEN_PATH,
+    auth =HTTPBasicAuth(USERNAME, getpass()),
+    verify=False
+)
 
-
+#print(post_response.json())                                                #DEBUG
+token = post_response.json()['data']['token']
+headers = {'X-Auth-Token': token, 'Content-Type': 'application/json'}
+print('Token recieved.')
+#print(f'Token: {token}')                                                   #DEBUG
 
 
 if __name__ == "__main__":
+    pass
 
-password = getpass()
